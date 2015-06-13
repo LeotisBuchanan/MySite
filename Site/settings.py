3e2b6e81b1ff974a09cafd7a684ff3d57b484862
Chinmaya <patanaikchinmaya@gmail.com>
2015-06-13 23:15:26 +0200

    sensitive information moved to a different settings file

--- Site/settings.py
+++ Site/settings.py
@@ -10,22 +10,15 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from settings_secret import *
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'o%_tb6nk#=c^+q-0_1i7_e&#lx_xv&#pvd#_*h^@p3h2zu8(82'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-TEMPLATE_DEBUG = True
-
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -69,17 +62,6 @@ WSGI_APPLICATION = 'Site.wsgi.application'
 CKEDITOR_UPLOAD_PATH = "uploads/"
 CKEDITOR_JQUERY_URL = '//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js'
 #CKEDITOR_IMAGE_BACKEND = 'pillow'
-
-
-# Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
-}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
---
+++ Site/settings_secret.py
@@ -0,0 +1,26 @@
+"""
+Django settings for Site project containing only sensitive information.
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'o%_tb6nk#=c^+q-0_1i7_e&#lx_xv&#pvd#_*h^@p3h2zu8(82'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+}
+
